773113ff55926834668af9a2712c2ef03bb79f8e
Luis Ugaz <luisugaz63@gmail.com>
2020-11-06 21:58:00 -0500

    javascript

---
+++ Sesion19/js/javascript.js
@@ -0,0 +1,17 @@
+function comapara() {
+    var a=2;
+    var b=2;
+    if (a==b) {
+        alert("a y b son iguales");
+    }else{
+        alert("a y b son diferentes");
+    }
+}
+function resta(x,y){
+    var resta=x-y;
+    document.write("<h2>la resta es"+ resta +"</h2>");
+}
+function suma(x,y){
+    var suma=x+y;
+    document.getElementById("sumar").innerHTML="La suma es "+suma;
+}
